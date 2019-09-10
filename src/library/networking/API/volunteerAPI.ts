9c2c7f53f65365045f3eb24562ed59e75b8983af
= <anirudh.b.123@gmail.com>
2019-09-10 17:18:18 +0530

    update assignEvent api

--- src/library/networking/API/volunteerAPI.ts
+++ src/library/networking/API/volunteerAPI.ts
@@ -1,26 +1,20 @@
 import constants from '../constants';
 import { BasicApiType } from '../../interfaces/BasicApi';
 
-export const assignEvent = (eventId: string, volunteerId: string, token: string) => {
-	fetch(constants.BASE_URL + "/volunteer/assignEvent", {
-		method: 'POST',
-		headers: constants.defaultHeaders,
-		body: JSON.stringify({ eventId, volunteerId, token })
-	})
-		.then(res => res.json())
-		.then((res: BasicApiType) => {
-			if (res.success) {
-				// Handle success
-				console.log('Successful');
-			} else {
-				// Handle error
-				console.log('error');
-			}
-		})
-		.catch(err => {
-			// Handle error
-			console.log('error');
+export const assignEvent = async (eventId: string, volunteerId: string, token: string) => {
+	let response: BasicApiType;
+	try {
+		let res = await fetch(constants.BASE_URL + "/volunteer/assignEvent", {
+			method: 'POST',
+			headers: constants.defaultHeaders,
+			body: JSON.stringify({ eventId, volunteerId, token })
 		});
+		response = await res.json();
+		return response;
+	} catch (err) {
+		response = { success: false, error: err };
+		return response;
+	}
 }
 
 export const unassignEvent = (eventId: string, volunteerId: string, token: string) => {
