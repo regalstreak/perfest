5c681e5d55a1073cd08a15d5af78afeaa9b0e2e3
Neil Agarwal <neil@neilagarwal.me>
2019-09-19 01:27:25 +0530

    constants: Add production server BASE_URL

--- src/library/networking/constants.ts
+++ src/library/networking/constants.ts
@@ -1,6 +1,7 @@
 const constants = {
     // BASE_URL: 'http://localhost:3001', //local
-    BASE_URL: 'https://perfest-backend-test.herokuapp.com', //heroku
+    // BASE_URL: 'https://perfest-backend-test.herokuapp.com', //heroku
+    BASE_URL: 'http://167.71.234.128:3000', //production server
     defaultHeaders: {
         "Content-Type": "application/json;charset=UTF-8"
     }
