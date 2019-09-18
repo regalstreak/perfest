5e509f5d21d98d4c81fa813d7267a0fb5e08e96d
Neil Agarwal <neil@neilagarwal.me>
2019-09-19 02:14:56 +0530

    constants: Get ssl with the backend server going

--- src/library/networking/constants.ts
+++ src/library/networking/constants.ts
@@ -1,7 +1,7 @@
 const constants = {
     // BASE_URL: 'http://localhost:3001', //local
     // BASE_URL: 'https://perfest-backend-test.herokuapp.com', //heroku
-    BASE_URL: 'http://167.71.234.128:3000', //production server
+    BASE_URL: 'https://backend.perfest.co/', //production server
     defaultHeaders: {
         "Content-Type": "application/json;charset=UTF-8"
     }
