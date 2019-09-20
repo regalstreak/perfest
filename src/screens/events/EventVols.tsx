5d9bd58ead087b5c829d76360404a448ebe50529
Neil Agarwal <neil@neilagarwal.me>
2019-09-21 03:09:39 +0530

    perfest-frontend: Display QR Scanner for scanning
    [SQUASHED] from branch develop
    6a3326aab28bd5b96b8efd9d8fa6b026d3004073^..8fb3eb4261377146e388bf8a65119f3712a2b802

---
+++ src/screens/events/EventVols.tsx
@@ -0,0 +1,87 @@
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, Platform } from 'react-native';
+import QrReader from 'react-qr-reader'
+import { invalidateTicket } from '../../library/networking/API/ticketAPI';
+import { useSelector } from 'react-redux';
+import { AppState } from '../../store/rootReducer';
+
+interface IEventVolsProps {
+
+}
+
+export default (props: IEventVolsProps) => {
+
+    const [qrResult, setQrResult] = useState<string>('');
+    const token = useSelector((state: AppState) => (state.auth.token));
+    const [uploading, setUploading] = useState<any>(null);
+    const [userData, setUserData] = useState<any>(null);
+    const [error, setError] = useState('');
+    const handleScan = async (data: string) => {
+        if (data) {
+            console.log(data);
+            setQrResult(data);
+            setUploading(<Text>Loading</Text>);
+            let res = await invalidateTicket(data, token);
+            setUploading(null);
+            // post data to server
+            // show if valid or not
+            if (res.success) {
+                // Handle success
+                console.log(res.ticketData);
+                setUserData(res.ticketData);
+            } else {
+                console.log(res.error);
+                setError(res.error);
+            }
+        }
+    }
+
+    let displayUser = null;
+    if (userData) {
+        displayUser = (
+            <View>
+                <Text>Event: {userData.event.name}</Text>
+                <Text>Paid: {userData.paid}</Text>
+                <Text>Validity: {userData.validity}</Text>
+                <Text>Email: {userData.user_id.contact.email}</Text>
+            </View>
+        )
+    }
+
+    let errorText;
+    if (error) {
+        errorText = <Text>An error occured !!! {error}</Text>
+    }
+
+    const handleError = (error: any) => {
+        console.log(error);
+    }
+
+    return (
+        <View style={styles.container}>
+            {uploading}
+            {Platform.OS === 'web' ?
+
+                <QrReader
+                    delay={1500}
+                    onError={(error: any) => { handleError(error) }}
+                    onScan={(data: any) => (handleScan(data))}
+                    style={{ width: '70%' }}
+                    showViewFinder={false}
+                /> : <Text>Abhi tak scanner implement nahi hua for native app</Text>
+            }
+
+            <Text>QR RESULT IS: {qrResult}</Text>
+            {errorText}
+            {displayUser}
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    }
+})
