15dffa3d821a63297c87d315841687c8521dfaf6
Neil Agarwal <neil@neilagarwal.me>
2019-09-21 01:00:04 +0530

    constants: Remove the extra tailing slash

--- src/library/networking/constants.ts
+++ src/library/networking/constants.ts
@@ -1,7 +1,7 @@
 const constants = {
     // BASE_URL: 'http://localhost:3001', //local
     // BASE_URL: 'https://perfest-backend-test.herokuapp.com', //heroku
-    BASE_URL: 'https://backend.perfest.co/', //production server
+    BASE_URL: 'https://backend.perfest.co', //production server
     defaultHeaders: {
         "Content-Type": "application/json;charset=UTF-8"
     }
