a3dc1fef37b7dfcee590970f3a1cd47e4b49ff20
Neil Agarwal <neil@neilagarwal.me>
2019-09-24 17:26:14 +0530

    Profile: Update User Profile, change name and stuff

--- src/library/res/styles.ts
+++ src/library/res/styles.ts
@@ -8,10 +8,20 @@ export const textStyles = StyleSheet.create({
         marginHorizontal: hp(3.5),
         marginTop: hp(4)
     },
+    NheaderText: {
+        fontSize: hp(4),
+        fontWeight: '600',
+        marginTop: hp(4)
+    },
     subHeaderText: {
         fontSize: hp(3.2),
         fontWeight: '600',
         marginHorizontal: hp(3.5),
+        marginTop: hp(2.6)
+    },
+    NsubHeaderText: {
+        fontSize: hp(3.2),
+        fontWeight: '600',
         marginTop: hp(2.6)
     },
 })
